b7715ea64eb6b7c4c43d540b3f8b2fb5f948c852
Goldn7799 <syeifsultan@gmail.com>
2022-01-13 07:14:17 +0000

    v2

--- src/XOODBQ77.js
+++ src/XOODBQ77.js
@@ -3,3 +3,6 @@ window.alert("Welcome To Sg Block")
 function start() {
     open("html/menu.html");
 }
+
+var name = prompt("Masukkan Nama Mu");
+document.write("<p> Selamat Datang <b>" + name + "</b></p");
